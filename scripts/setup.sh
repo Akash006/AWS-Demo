9e2789559cc0e080255a35dfad49c6f6a7543c4d
Akash Saini <mastermindvishu97@gmail.com>
2026-04-25 00:39:37 +0530

    Update setup script to include git and pm2
    Added git installation and pm2 to the setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 yum update
-yum install -y nginx nodejs npm
+yum install -y nginx nodejs npm git 
 
 git clone https://github.com/Akash006/AWS-Demo.git
 cd AWS-Demo
 
 npm init -y
-npm install express mysql2 pg aws-sdk multer dotenv @aws-sdk/client-dynamodb @aws-sdk/lib-dynamodb
+npm install express mysql2 pg aws-sdk multer dotenv @aws-sdk/client-dynamodb @aws-sdk/lib-dynamodb pm2
