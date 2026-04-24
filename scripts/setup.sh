f3d33901510c6748c4bf095012dfab57f5aabb2f
root <root@ip-172-31-8-215.ap-south-1.compute.internal>
2026-04-24 18:25:04 +0000

    Adding scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yum update
+yum install -y nginx nodejs npm
+
+git clone https://github.com/Akash006/AWS-Demo.git
+cd AWS-Demo
+
+npm init -y
+npm install express mysql2 pg aws-sdk multer dotenv
---
+++ scripts/stress.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while true; do
+  curl http://YOUR_LOAD_BALANCER_URL/api/health
+done
