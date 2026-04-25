f06966df2aa10e5b8e0150d906e00eefb2f72e4b
Akash Saini <mastermindvishu97@gmail.com>
2026-04-25 21:19:17 +0530

    Update setup script to install pm2 globally

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,12 @@
 yum update
 yum install -y nginx nodejs npm git 
 
+cd /opt
 git clone https://github.com/Akash006/AWS-Demo.git
 cd AWS-Demo
 
 npm init -y
-npm install express mysql2 pg aws-sdk multer dotenv @aws-sdk/client-dynamodb @aws-sdk/lib-dynamodb pm2
+npm install express mysql2 pg aws-sdk multer dotenv @aws-sdk/client-dynamodb @aws-sdk/lib-dynamodb
+npm install pm2 -g
 
 pm2 start app/server.js --name quickbites
